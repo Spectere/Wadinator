290dd60ed9fd94268e06a41035b74a310b7f5eda
Ian Burgmyer <spectere@gmail.com>
2022-05-24 18:33:05 -0400

    Added basic Heretic support.

--- Wadinator/Program.cs
+++ Wadinator/Program.cs
@@ -3,6 +3,57 @@ using Wadinator;
 
 const string playedFileName = "wadinator_played.txt";
 
+static (CompLevel, bool) DetectCompLevel(ref WadReader wad, ref List<WadReader.WadDirectoryEntry> mapList, bool hasDoom2Maps) {
+    // Perform a linedef/sector analysis to guess the correct complevel.
+    var compLevel = CompLevel.Doom19;
+    foreach(var lump in wad.Lumps) {
+        // Indiscriminately check all supported lumps.
+        var resultComplevel = lump.Name switch {
+            "DEHACKED" => Lumpalyzer.AnalyzeDeHackEd(wad.GetLump(lump)),
+            "LINEDEFS" => Lumpalyzer.AnalyzeLinedefs(wad.GetLump(lump)),
+            "SECTORS" => Lumpalyzer.AnalyzeSectors(wad.GetLump(lump)),
+            "THINGS" => Lumpalyzer.AnalyzeThings(wad.GetLump(lump)),
+            _ => compLevel
+        };
+
+        compLevel = compLevel.Promote(resultComplevel);
+    }
+
+    /*
+     * Perform any necessary complevel adjustments.
+     */
+    var hasMismatchedBosses = false;
+    if(compLevel < CompLevel.Boom && !hasDoom2Maps) {
+        // If Ultimate Doom maps are detected, we already have a good idea which
+        // complevel this should be.
+        var ultimateDoomMapRegEx = new Regex("^(E4M.)$");
+        if(mapList.Any(map => ultimateDoomMapRegEx.IsMatch(map.Name))) {
+            compLevel = CompLevel.UltimateDoom;
+            return (compLevel, hasMismatchedBosses);
+        }
+
+        /*
+         * Check for mismatched boss enemies (Cyberdemon/Mastermind on E1M8).
+         */
+        if(mapList.Any(lump => lump.Name == "E1M8")) {
+            var mapLumps = GetMapLumps(wad, "E1M8");
+            var sectorLump = mapLumps.FirstOrDefault(x => x.Name == "SECTORS");
+            var thingLump = mapLumps.FirstOrDefault(x => x.Name == "THINGS");
+
+            if(sectorLump is not null && thingLump is not null) {
+                var sectorStream = wad.GetLump(sectorLump);
+                var thingStream = wad.GetLump(thingLump);
+
+                hasMismatchedBosses = Lumpalyzer.HasMismatchedBossEncounter("E1M8", sectorStream, thingStream);
+            }
+        }
+
+        compLevel = compLevel.Promote(CompLevel.UltimateDoom, !hasMismatchedBosses);
+    }
+
+    return (compLevel, hasMismatchedBosses);
+}
+
 static List<WadReader.WadDirectoryEntry> GetMapLumps(in WadReader wad, string mapName) {
     // NOTE: If we need to fetch more items, expand this list accordingly.
     var candidates = new List<string> {
@@ -76,7 +127,7 @@ static void Print(params string[] lines) {
 
 if(args.Length < 1 || args.Length > 2) {
     var startCommand = Environment.CommandLine.Split(' ')[0];
-    Print($"usage: {startCommand} [-r] <path/file>",
+    Print($"usage: {startCommand} [-r] [-heretic] <path/file>",
         "",
         "  If a directory is specified, a WAD will be randomly selected and logged to",
         "  ensure that the same file is not picked twice. If a file is selected, the WAD",
@@ -84,29 +135,49 @@ if(args.Length < 1 || args.Length > 2) {
         "  if you only want the program to estimate the appropriate complevel for a single",
         "  file.",
         "",
-        "    -r    Scan directory recursively (only valid if a directory is specified).",
+        "    -r        Scan directory recursively (only valid if a directory is specified).",
+        "    -heretic  Specifies that the WADs in the directory were designed for Heretic.",
         ""
     );
     return 1;
 }
 
-// Handle the recursion parameter as lazily as possible.
+// Handle command line parameters as lazily as possible.
 var basePathIndex = 0;
 var recurse = false;
-if(args.Length > 1) {
-    if(args[0].ToLower() == "-r") {
-        basePathIndex = 1;
-        recurse = true;
-    } else if(args[1].ToLower() == "-r") {
-        recurse = true;
-    } else {
-        Print("error: unknown parameter.");
-        return 1;
+var heretic = false;
+var path = "";
+foreach(var arg in args) {
+    switch(arg) {
+        case "-r":
+            recurse = true;
+            break;
+
+        case "-heretic":
+            heretic = true;
+            break;
+
+        default:
+            if(arg.StartsWith("-")) {
+                Print("error: unknown parameter");
+                return 1;
+            }
+
+            if(!string.IsNullOrWhiteSpace(path)) {
+                Print(
+                    "error: only one path/filename can be specified at a time!",
+                    "",
+                    "If the path has spaces in it, be sure to surround it in quotes.");
+
+                return 1;
+            }
+
+            path = arg;
+            break;
     }
 }
 
 // Determine if the passed path is a file or directory (or if it doesn't exist at all).
-var path = args[basePathIndex];
 var pathIsDirectory = false;
 
 if(Directory.Exists(path)) {
@@ -135,53 +206,12 @@ var mapList = wad.Lumps.Where(x => allMapsRegEx.IsMatch(x.Name)).ToList();
 var hasDoom1Maps = mapList.Any(x => doom1MapRegEx.IsMatch(x.Name));
 var hasDoom2Maps = mapList.Any(x => x.Name.StartsWith("MAP"));
 
-// Perform a linedef/sector analysis to guess the correct complevel.
 var compLevel = CompLevel.Doom19;
-foreach(var lump in wad.Lumps) {
-    // Indiscriminately check all supported lumps.
-    var resultComplevel = lump.Name switch {
-        "DEHACKED" => Lumpalyzer.AnalyzeDeHackEd(wad.GetLump(lump)),
-        "LINEDEFS" => Lumpalyzer.AnalyzeLinedefs(wad.GetLump(lump)),
-        "SECTORS" => Lumpalyzer.AnalyzeSectors(wad.GetLump(lump)),
-        "THINGS" => Lumpalyzer.AnalyzeThings(wad.GetLump(lump)),
-        _ => compLevel
-    };
-
-    compLevel = compLevel.Promote(resultComplevel);
-}
-
-/*
- * Perform any necessary complevel adjustments.
- */
 var hasMismatchedBosses = false;
-if(compLevel < CompLevel.Boom && !hasDoom2Maps) {
-    // If Ultimate Doom maps are detected, we already have a good idea which
-    // complevel this should be.
-    var ultimateDoomMapRegEx = new Regex("^(E4M.)$");
-    if(mapList.Any(map => ultimateDoomMapRegEx.IsMatch(map.Name))) {
-        compLevel = CompLevel.UltimateDoom;
-        goto endCompLevelAdjustment;  // You saw nothing.
-    }
 
-    /*
-     * Check for mismatched boss enemies (Cyberdemon/Mastermind on E1M8).
-     */
-    if(mapList.Any(lump => lump.Name == "E1M8")) {
-        var mapLumps = GetMapLumps(wad, "E1M8");
-        var sectorLump = mapLumps.FirstOrDefault(x => x.Name == "SECTORS");
-        var thingLump = mapLumps.FirstOrDefault(x => x.Name == "THINGS");
-
-        if(sectorLump is not null && thingLump is not null) {
-            var sectorStream = wad.GetLump(sectorLump);
-            var thingStream = wad.GetLump(thingLump);
-
-            hasMismatchedBosses = Lumpalyzer.HasMismatchedBossEncounter("E1M8", sectorStream, thingStream);
-        }
-    }
-
-    compLevel = compLevel.Promote(CompLevel.UltimateDoom, !hasMismatchedBosses);
+if(!heretic) {
+    (compLevel, hasMismatchedBosses) = DetectCompLevel(ref wad, ref mapList, hasDoom2Maps);
 }
-endCompLevelAdjustment:
 
 // Add the WAD to the played file.
 if(pathIsDirectory) {
@@ -211,72 +241,83 @@ if(pathIsDirectory) {
 
 Print(
     "",
-     "  This WAD contains the following maps:",
-     "",
-    $"    {string.Join(", ", mapList.OrderBy(lump => lump.Name).Select(lump => lump.Name))}",
-     "",
-     "",
-     "  Here, have a convenient command line:",
-     "",
-    $"    {exePrefix}dsda-doom -iwad {(hasDoom2Maps ? "doom2.wad" : "doom.wad")} -file {path} -complevel {(int)compLevel} -skill 4",
-     ""
+    "  This WAD contains the following maps:",
+    "",
+   $"    {string.Join(", ", mapList.OrderBy(lump => lump.Name).Select(lump => lump.Name))}",
+    "",
+    "",
+    "  Here, have a convenient command line:",
+    ""
 );
 
-if(hasMismatchedBosses) {
+if(heretic) {
+    // Heretic
     Print(
-        "",
-        "  NOTE: This WAD has a cyberdemon and/or spider mastermind on E1M8, as well as sectors",
-        "        with the 666 tag value. If this WAD was designed around Ultimate Doom 1.9's",
-        "        behavior, this will likely lead to unexpected results. In this case, use complevel",
-        "        3. If this is an older WAD that relies on the legacy behavior (such as UAC_DEAD.WAD),",
-        "        complevel 2 (as specified above) must be used.",
+        $"    {exePrefix}crispy-heretic -file {path} -skill 4"
+    );
+} else {
+    // Doom or Doom II
+    Print(
+       $"    {exePrefix}dsda-doom -iwad {(hasDoom2Maps ? "doom2.wad" : "doom.wad")} -file {path} -complevel {(int)compLevel} -skill 4",
         ""
     );
-}
 
-if(compLevel < CompLevel.Mbf21) {
-    Print(
-         "",
-        $"  The complevel detection isn't perfect. If you run into trouble, try {(compLevel == CompLevel.Mbf ? "this" : "one of these")} instead:",
-         ""
-    );
-}
-
-if(compLevel < CompLevel.Boom && hasDoom1Maps) {
-    Print(
-        "     0 - Doom v1.2",
-        "     1 - Doom v1.666");
-
-    if(compLevel == CompLevel.UltimateDoom) {
+    if(hasMismatchedBosses) {
         Print(
-            "     2 - Doom v1.9"
+            "",
+            "  NOTE: This WAD has a cyberdemon and/or spider mastermind on E1M8, as well as sectors",
+            "        with the 666 tag value. If this WAD was designed around Ultimate Doom 1.9's",
+            "        behavior, this will likely lead to unexpected results. In this case, use complevel",
+            "        3. If this is an older WAD that relies on the legacy behavior (such as UAC_DEAD.WAD),",
+            "        complevel 2 (as specified above) must be used.",
+            ""
         );
     }
 
-    if(compLevel == CompLevel.Doom19) {
+    if(compLevel < CompLevel.Mbf21) {
         Print(
-            "     3 - Ultimate Doom v1.9"
+            "", 
+           $"  The complevel detection isn't perfect. If you run into trouble, try {(compLevel == CompLevel.Mbf ? "this" : "one of these")} instead:",
+            ""
         );
     }
-}
 
-if(compLevel < CompLevel.Boom && hasDoom2Maps) {
-    Print(
-        "     1 - Doom v1.666",
-        "     4 - Final Doom"
-    );
-}
+    if(compLevel < CompLevel.Boom && hasDoom1Maps) {
+        Print(
+            "     0 - Doom v1.2",
+            "     1 - Doom v1.666");
 
-if(compLevel < CompLevel.Boom)
-    Print("     9 - Boom");
-if(compLevel < CompLevel.Mbf)
-    Print("    11 - MBF");
-if(compLevel < CompLevel.Mbf21) {
-    Print(
-        "    21 - MBF21",
-        "",
-        "  When in doubt, check the WAD's readme!"
-    );
+        if(compLevel == CompLevel.UltimateDoom) {
+            Print(
+                "     2 - Doom v1.9"
+            );
+        }
+
+        if(compLevel == CompLevel.Doom19) {
+            Print(
+                "     3 - Ultimate Doom v1.9"
+            );
+        }
+    }
+
+    if(compLevel < CompLevel.Boom && hasDoom2Maps) {
+        Print(
+            "     1 - Doom v1.666",
+            "     4 - Final Doom"
+        );
+    }
+
+    if(compLevel < CompLevel.Boom)
+        Print("     9 - Boom");
+    if(compLevel < CompLevel.Mbf)
+        Print("    11 - MBF");
+    if(compLevel < CompLevel.Mbf21) {
+        Print(
+            "    21 - MBF21",
+            "",
+            "  When in doubt, check the WAD's readme!"
+        );
+    }
 }
 
 Print("");
