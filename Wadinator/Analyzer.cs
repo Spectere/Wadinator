Spectere/Wadinator

commit e1e9896fee98321ac8bd2d81f571587221cf67f2

Map name is handled more like Doom (fixes #4).
* Map names are split by episode to make the output cleaner for megawads.

Wadinator/Analyzer.cs
@@ -11,9 +11,9 @@ public class Analyzer {
     private AnalysisSettings _analysisSettings;
 
     // Map List Regex
-    private static readonly Regex AllMapsRegEx = new("^(E.M.|MAP..)$");
-    private static readonly Regex EpisodeAndMapRegEx = new("^(E.M.)$");
-    private static readonly Regex UltimateDoomMapRegEx = new Regex("^(E4M.)$");
+    private static readonly Regex AllMapsRegEx = new("^(E.M.|MAP..)");
+    private static readonly Regex EpisodeAndMapRegEx = new("^(E.M.)");
+    private static readonly Regex UltimateDoomMapRegEx = new Regex("^(E4M.)");
 
     // Map Lump Information
     private List<WadDirectoryEntry> MapList =>
@@ -170,7 +170,7 @@ public class Analyzer {
 
         // Fetch the base index of the map lump.
         for(var i = 0; i < _wad.Lumps.Count; i++) {
-            if(_wad.Lumps[i].Name != mapName) {
+            if(!_wad.Lumps[i].Name.StartsWith(mapName)) {
                 continue;
             }
 

Wadinator/AnalysisResults.cs
@@ -1,23 +1,126 @@
+using System.Text;
+
 namespace Wadinator;
 
 /// <summary>
 /// Contains the results of a WAD analysis.
 /// </summary>
-/// <param name="CompLevel">The detected <see cref="CompLevel"/> for this WAD.</param>
-/// <param name="ContainsExMxMaps"><c>true</c> if this WAD contains maps with ExMx map lumps, otherwise <c>false</c>.</param>
-/// <param name="ContainsMapXxMaps"><c>true</c> if this WAD contains maps with MAPxx map lumps, otherwise <c>false</c>.</param>
-/// <param name="HasMismatchedBosses"><c>true</c> if this WAD contains Cyberdemon(s) or Spider Mastermind(s), as well as sectors with
-/// tag 666, in E1M8.</param>
-/// <param name="MapList">A list of maps lumps contained in this WAD.</param>
-/// <param name="MusicList">A list of music lumps contained in this WAD.</param>
-/// <param name="IsDeathmatchWad"><c>true</c> if this WAD is flagged as a deathmatch WAD. This will be set to <c>false</c> if
-/// deathmatch WAD detection is disabled, or if the check returns negative.</param>
-public record AnalysisResults(
-    CompLevel CompLevel,
-    bool ContainsExMxMaps,
-    bool ContainsMapXxMaps,
-    bool HasMismatchedBosses,
-    List<WadDirectoryEntry> MapList,
-    List<WadDirectoryEntry> MusicList,
-    bool IsDeathmatchWad
-);
+public record AnalysisResults {
+    /// <summary>The detected <see cref="CompLevel"/> for this WAD.</summary>
+    public CompLevel CompLevel { get; }
+
+    /// <summary><c>true</c> if this WAD contains maps with ExMx map lumps, otherwise <c>false</c>.</summary>
+    public bool ContainsExMxMaps { get; }
+
+    /// <summary><c>true</c> if this WAD contains maps with MAPxx map lumps, otherwise <c>false</c>.</summary>
+    public bool ContainsMapXxMaps { get; }
+
+    /// <summary><c>true</c> if this WAD contains Cyberdemon(s) or Spider Mastermind(s), as well as sectors with
+    /// tag 666, in E1M8.</summary>
+    public bool HasMismatchedBosses { get; }
+
+    /// <summary>A list of maps lumps contained in this WAD.</summary>
+    public List<WadDirectoryEntry> MapList { get; }
+
+    /// <summary>A list of music lumps contained in this WAD.</summary>
+    public List<WadDirectoryEntry> MusicList { get; }
+
+    /// <summary><c>true</c> if this WAD is flagged as a deathmatch WAD. This will be set to <c>false</c> if
+    /// deathmatch WAD detection is disabled, or if the check returns negative.</summary>
+    public bool IsDeathmatchWad { get; }
+    
+    /// <summary>
+    /// Contains the results of a WAD analysis.
+    /// </summary>
+    /// <param name="CompLevel">The detected <see cref="CompLevel"/> for this WAD.</param>
+    /// <param name="ContainsExMxMaps"><c>true</c> if this WAD contains maps with ExMx map lumps, otherwise <c>false</c>.</param>
+    /// <param name="ContainsMapXxMaps"><c>true</c> if this WAD contains maps with MAPxx map lumps, otherwise <c>false</c>.</param>
+    /// <param name="HasMismatchedBosses"><c>true</c> if this WAD contains Cyberdemon(s) or Spider Mastermind(s), as well as sectors with
+    /// tag 666, in E1M8.</param>
+    /// <param name="MapList">A list of maps lumps contained in this WAD.</param>
+    /// <param name="MusicList">A list of music lumps contained in this WAD.</param>
+    /// <param name="IsDeathmatchWad"><c>true</c> if this WAD is flagged as a deathmatch WAD. This will be set to <c>false</c> if
+    /// deathmatch WAD detection is disabled, or if the check returns negative.</param>
+    public AnalysisResults(CompLevel CompLevel,
+                           bool ContainsExMxMaps,
+                           bool ContainsMapXxMaps,
+                           bool HasMismatchedBosses,
+                           List<WadDirectoryEntry> MapList,
+                           List<WadDirectoryEntry> MusicList,
+                           bool IsDeathmatchWad) {
+        this.CompLevel = CompLevel;
+        this.ContainsExMxMaps = ContainsExMxMaps;
+        this.ContainsMapXxMaps = ContainsMapXxMaps;
+        this.HasMismatchedBosses = HasMismatchedBosses;
+        this.MapList = MapList;
+        this.MusicList = MusicList;
+        this.IsDeathmatchWad = IsDeathmatchWad;
+    }
+
+    /// <summary>
+    /// This adjusts the Doom II MAPxx name so that the episodes are separated appropriately.
+    /// If we don't do this, we'll end up with MAP01-09 (9 maps) on one line and MAP10-19 (10
+    /// maps) on the next.
+    /// </summary>
+    /// <param name="mapName">The name of the map to adjust.</param>
+    /// <returns>The adjusted map name. This will be the normal map name with the map number
+    /// decremented by one. If an error occurs, the original map name will be returned.</returns>
+    private string AdjustDoom2MapNameForEpisodeSort(string mapName) =>
+        int.TryParse(mapName.AsSpan(3, 2), out var mapNumber)
+            ? $"MAP{mapNumber - 1:00}"
+            : mapName;
+
+    /// <summary>
+    /// Returns a formatted list of maps.
+    /// </summary>
+    /// <param name="padding">The number of spaces of padding to put before each line.</param>
+    /// <returns>A string containing a formatted list of maps.</returns>
+    public string GetFormattedMapList(int padding) {
+        StringBuilder output = new();
+        var pad = new string(' ', padding);
+        
+        if(ContainsExMxMaps) {
+            // Put each episode on its own line.
+            var prefixes = MapList.Select(x => x.Name)
+                                  .Where(x => x.StartsWith("E"))
+                                  .Select(x => x[..2])
+                                  .Distinct()
+                                  .OrderBy(x => x);
+
+            foreach(var prefix in prefixes) {
+                // Find the maps in each episode, trim the strings to remove any excess junk, and display them in a nice list.
+                var episodeMaps = MapList.Select(x => x.Name)
+                                         .Where(x => x.StartsWith(prefix))
+                                         .Select(x => x[..4])
+                                         .Distinct()
+                                         .OrderBy(x => x);
+
+                output.Append(pad);
+                output.AppendLine(string.Join(", ", episodeMaps));
+            }
+        }
+
+        if(ContainsMapXxMaps) {
+            // Put each "episode" (10 map set) on its own line.
+            var prefixes = MapList.Select(x => x.Name)
+                                  .Where(x => x.StartsWith("MAP"))
+                                  .Select(x => AdjustDoom2MapNameForEpisodeSort(x)[..4])
+                                  .Distinct()
+                                  .OrderBy(x => x);
+
+            foreach(var prefix in prefixes) {
+                // Find the maps in each "episode," trim the strings down to remove any excess junk, and display them in a nice list.
+                var episodeMaps = MapList.Select(x => x.Name)
+                                         .Where(x => AdjustDoom2MapNameForEpisodeSort(x).StartsWith(prefix))
+                                         .Select(x => x[..5])
+                                         .Distinct()
+                                         .OrderBy(x => x);
+
+                output.Append(pad);
+                output.AppendLine(string.Join(", ", episodeMaps));
+            }
+        }
+
+        return output.ToString();
+    }
+}

Wadinator/Program.cs
@@ -540,7 +540,7 @@ if(analysisResults.MapList.Any()) {
     var firstMap = analysisResults.MapList.MinBy(x => x.Name)!;
 
     if(firstMap.Name.StartsWith("MAP")) {
-        warpString += firstMap.Name[3..].TrimStart('0');
+        warpString += firstMap.Name.Substring(3, 2).TrimStart('0');
     } else {
         warpString += $"{firstMap.Name[1]} {firstMap.Name[3]}";
     }
@@ -576,8 +576,7 @@ Print(
     "",
     "  This WAD contains the following maps:",
     "",
-   $"    {string.Join(", ", analysisResults.MapList.OrderBy(lump => lump.Name).Select(lump => lump.Name))}",
-    "",
+    analysisResults.GetFormattedMapList(4),
     "",
     "  Here, have a convenient command line:",
     ""

commit 06b61741acc89ce8f8e013e0faf39cbb3c43e452

Strengthened map marker lump detection.
* Corrects a regression introduced in e1e9896.
* Prevents the MAPINFO lump from being treated as a map marker. Whoopsie.

Wadinator/Analyzer.cs
@@ -11,9 +11,10 @@ public class Analyzer {
     private AnalysisSettings _analysisSettings;
 
     // Map List Regex
-    private static readonly Regex AllMapsRegEx = new("^(E.M.|MAP..)");
-    private static readonly Regex EpisodeAndMapRegEx = new("^(E.M.)");
-    private static readonly Regex UltimateDoomMapRegEx = new Regex("^(E4M.)");
+    private static readonly Regex AllMapsRegEx = new(@"^(E\dM\d|MAP\d\d).*");
+    private static readonly Regex EpisodeAndMapRegEx = new(@"^(E\dM\d).*");
+    private static readonly Regex UltimateDoomMapRegEx = new Regex(@"^(E4M\d).*");
+    private static readonly Regex MapRegex = new(@"^MAP\d\d.*");
 
     // Map Lump Information
     private List<WadDirectoryEntry> MapList =>
@@ -21,7 +22,7 @@ public class Analyzer {
 
     private bool HasUltimateDoomMaps => MapList.Any(map => UltimateDoomMapRegEx.IsMatch(map.Name));
     private bool UsesEpisodeAndMap => MapList.Any(x => EpisodeAndMapRegEx.IsMatch(x.Name));
-    private bool UsesMapOnly => MapList.Any(x => x.Name.StartsWith("MAP"));
+    private bool UsesMapOnly => MapList.Any(x => MapRegex.IsMatch(x.Name));
     
     // Map Lump Directory Entries
     private struct MapLumpCollection {